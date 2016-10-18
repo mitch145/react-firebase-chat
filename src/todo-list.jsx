3dc178ae38f2fd2e12aa515089af12f66a1d9075
Mitch Ball <mitchb145@gmail.com>
2016-10-18 15:10:28 +1100

    Chat view is generated from static test array.

--- src/todo-list.jsx
+++ src/todo-list.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import Todo from './todo';
 
+var messages = [
+  {name: 'Mitch', message: 'Hello World'},
+  {name: 'Mitch', message: 'What a nice day'},
+  {name: 'AI', message: 'Hello Mitch'},
+  {name: 'Mitch', message: 'How\'s it going?'}
+]
+
+// var messageList = this.props.messages.map(function(message) {
+var messageList = messages.map(function(message) {
+  return (
+    <Todo name={message.name} message={message.message} />
+  );
+});
+
 class TodoList extends React.Component {
+
     render() {
         return (
-          <div className="container todoList">
+          <div className="container message-list">
             <div className="row">
               <div className="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div className="panel panel-default">
                   <div className="panel-heading">
-                    <h1 className="text-center">Mitch's Todo List</h1>
+                    <h1 className="text-center">Mitch's Chat App</h1>
                   </div>
-                  <Todo/>
+                  <div className="panel-body">
+                  {messageList}
+                  </div>
                 </div>
               </div>
             </div>
--- src/todo.jsx
+++ src/todo.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 class Todo extends React.Component{
   render (){
     return (
-      <div className="panel-body">
-        Panel content
+      <div className="message row">
+        <p className={((this.props.name == 'Mitch'))? 'self' : 'friend'}>{this.props.name}: {this.props.message}</p>
       </div>
     );
   }
